66eee1d5969da336e5be817f17e72d1d653b97fd
Olive Mugeri <olive.nyotu@andela.com>
2018-01-30 16:46:55 +0300

    two factor code

--- connect.js
+++ connect.js
@@ -1,14 +1,20 @@
 const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+require('dotenv').config()
+
 const client =  new SteamUser();
-import { PASSWORD } from 'dotenv';
 
 const logOnOptions = {
   accountName: 'riwhiz',
-  password: PASSWORD,
+  password: process.env.PASSWORD,
+  // twoFactorCode: SteamTotp.generateAuthCode(process.env.CODE),
 };
 
 client.logOn(logOnOptions);
 
 client.on('loggedOn', ()=> {
   console.log('Logged into Steam');
+
+  client.setPersona(SteamUser.EPersonaState.Online);
+  client.gamesPlayed(440);
 })
